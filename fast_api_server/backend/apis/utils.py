159c1e40d1490a83f469b3355a7f02637d7c2093
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2022-10-06 11:45:22 +0200

    Update utils.py

--- fast_api_server/backend/apis/utils.py
+++ fast_api_server/backend/apis/utils.py
@@ -24,10 +24,10 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
         )  # changed to accept access token from httpOnly Cookie
 
         scheme, param = get_authorization_scheme_param(authorization)
-        print()
-        print(request.cookies)
-        print(authorization)
-        print()
+        # print()
+        # print(request.cookies)
+        # print(authorization)
+        # print()
         if not authorization or scheme.lower() != "bearer":
             if self.auto_error:
                 raise HTTPException(
