01ac59597f69443b23fa50d5ef749a4e186e1541
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2022-11-04 12:04:15 +0100

    Update add_row.py

--- backend/SQL/add_row.py
+++ backend/SQL/add_row.py
@@ -1,18 +1,8 @@
-# import db
 import SQL.db as db
 from sqlalchemy.orm import sessionmaker
 
-
-# def makeNewSession():
-# class Session:
 def getSession():
     #new session
     Session = sessionmaker(bind = db.engine)
     session = Session()
-    # session = sessionmaker(bind=db.engine)
     return session
-
-# def addRowToDb(_row, session):
-#     row = db.CallList(_row)
-#     session.add(row)
-#     session.commit()
