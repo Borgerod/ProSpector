7156fa99a777e57229b968de3d9472a6bf46ae64
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2022-12-05 15:08:31 +0100

    deleting stuff from user_backend
    config.py -> PROJECT_VERSION & SECRET_KEY & TEST_USER_EMAIL
    /templates/

--- backend/user_backend/fast_api_server/core/config.py
+++ backend/user_backend/fast_api_server/core/config.py
@@ -1,4 +1,4 @@
-# Nessasary for importing config 
+''' Nessasary for importing config ''' 
 import sys; sys.path.insert(0, '.')
 from virtual_env.config import Config
 
@@ -8,23 +8,21 @@ class Settings:
     '''
     config = Config.getPostgresConfig # Get Appropriate config variables 
     
-    PROJECT_NAME: str = "ProSpector"
-    PROJECT_VERSION: str = "1.0.0" #todo REMOVE THIS (could replace with func that finds real version)
+    #> Test: removing PROJECT_VERSION & SECRET_KEY & TEST_USER_EMAIL
+    #todo REMOVE THIS (could replace with func that finds real version)
+    # PROJECT_VERSION: str = "1.0.0" 
+    # SECRET_KEY: str = config["SECRET_KEY"] #NOT IN ISE
+    # TEST_USER_EMAIL = "test@example.com"
 
+    PROJECT_NAME: str = "ProSpector"
     USE_SQLITE_DB: str = config["USE_SQLITE_DB"]
-    # POSTGRES_USER: str = config["POSTGRES_USER"]
-    POSTGRES_USER = "postgres"
+    POSTGRES_USER = "postgres" #(OR) POSTGRES_USER: str = config["POSTGRES_USER"] 
     POSTGRES_PASSWORD = config["POSTGRES_PASSWORD"]
-    POSTGRES_SERVER: str = config["POSTGRES_SERVER"]#, "localhost",
-    POSTGRES_PORT: str = config["POSTGRES_PORT"]#, 5432,
-      # default postgres port is 5432
-    POSTGRES_DB: str = config["POSTGRES_DB"]#, "tdd",
+    POSTGRES_SERVER: str = config["POSTGRES_SERVER"] #(OR), "localhost",
+    POSTGRES_PORT: str = config["POSTGRES_PORT"] #(OR), 5432, ''' default postgres port is 5432 '''
+    POSTGRES_DB: str = config["POSTGRES_DB"]  #(OR), "tdd",
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    SECRET_KEY: str = config["SECRET_KEY"]
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 480  # in mins (8 hours)
-
-    TEST_USER_EMAIL = "test@example.com"
-
 
 settings = Settings()
