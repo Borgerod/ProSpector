f65fe65d41d3c686c8ab35e06e4244e293ea0bc3
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2022-12-05 15:17:29 +0100

    Update config.py

--- backend/dev_backend/SQL/config.py
+++ backend/dev_backend/SQL/config.py
@@ -22,35 +22,32 @@ from sqlalchemy.orm.decl_api import DeclarativeMeta #for annotation
 
 class DevSettings:
 	def __init__(self) -> None:
-		# self.POSTGRES_USER = "mediavest"	#TEMP while testing (env variables seems to be wrong)
-		self.POSTGRES_USER = "postgres"	#TEMP while testing (env variables seems to be wrong)
-		# self.POSTGRES_USER = os.environ["POSTGRES_USER"] # TEMPORARLY DISABLED WHILE TESTING
-		# TEMP
+		''' ___ Postgres ___ '''
+		self.POSTGRES_SERVER = "localhost"
+		self.POSTGRES_PORT = os.environ["POSTGRES_PORT"]
+		self.POSTGRES_DB = os.environ["POSTGRES_DB"] #(OR) self.POSTGRES_DB = "ProSpector_Dev"
+		self.SECRET_KEY = os.environ["SECRET_KEY"]
+		self.USE_SQLITE_DB = os.environ["USE_SQLITE_DB"]
+		self.POSTGRES_USER = os.environ["POSTGRES_USER"] #(OR) self.POSTGRES_USER = "postgres" (OR) "mediavest"
 		try:
 			self.POSTGRES_DEV_PASSWORD = os.environ["POSTGRES_DEV_PASSWORD"]
 		except:
 			print("Could not connect to Postgres, edit password in: ProSpector\backend\dev_backend\SQL\config.py")
 			"""self.POSTGRES_DEV_PASSWORD ="Mitt personlige passord"""
-
-		self.POSTGRES_SERVER = "localhost"
-		self.POSTGRES_PORT = os.environ["POSTGRES_PORT"]
-		# self.POSTGRES_DB = os.environ["POSTGRES_DB"]
-		self.POSTGRES_DB = "ProSpector_Dev"
-		self.SECRET_KEY = os.environ["SECRET_KEY"]
-		self.USE_SQLITE_DB = os.environ["USE_SQLITE_DB"]
 		self.DATABASE_URL = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_DEV_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
+		
+		''' ___ EMAIL ___ '''
 		self.MAIL_FROM = os.environ["MAIL_FROM"]
 		self.MAIL_PORT = os.environ["MAIL_PORT"]
 		self.MAIL_SERVER = os.environ["MAIL_SERVER"]
 		self.MAIL_FROM_NAME = os.environ["MAIL_SERVER"]
 		self.MAIL_USERNAME = os.environ["MAIL_USERNAME"]
 		self.MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
-		# self.MAIL_TSL = os.environ["MAIL_TSL"]
-		# self.MAIL_SSL = os.environ["MAIL_SSL"]
 		self.MAIL_TSL = os.environ["MAIL_TSL"]
 		self.MAIL_SSL = os.environ["MAIL_SSL"]
 		self.USE_CREDENTIALS = os.environ["USE_CREDENTIALS"]	
+
+		''' ___ TWILIO ___ '''
 		self.TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
 		self.TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 		self.VERIFY_SID = 'VA0af259d1f90eef02d4cfe86f45e64b8f'	
@@ -69,32 +66,34 @@ class Dev:
 
 class Settings:   
 	def __init__(self) -> None:
-		# self.POSTGRES_USER = "mediavest"	#TEMP while testing (env variables seems to be wrong)
-		self.POSTGRES_USER = "postgres"	#TEMP while testing (env variables seems to be wrong)
-		# self.POSTGRES_USER = os.environ["POSTGRES_USER"] # TEMPORARLY DISABLED WHILE TESTING
-		self.POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
-		self.POSTGRES_SERVER = os.environ["POSTGRES_SERVER"]
+		''' ___ Postgres ___ '''
+		self.POSTGRES_SERVER = "localhost"
 		self.POSTGRES_PORT = os.environ["POSTGRES_PORT"]
-		# self.POSTGRES_DB = os.environ["POSTGRES_DB"]
-		self.POSTGRES_DB = "ProSpector_Dev"
-		self.SECRET_KEY = os.environ["SECRET_KEY"]
+		self.POSTGRES_DB = os.environ["POSTGRES_DB"] #(OR) self.POSTGRES_DB = "ProSpector_Dev"
 		self.USE_SQLITE_DB = os.environ["USE_SQLITE_DB"]
-		self.DATABASE_URL = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
+		self.POSTGRES_USER = os.environ["POSTGRES_USER"] #(OR) self.POSTGRES_USER = "postgres" (OR) "mediavest"
+		try:
+			self.POSTGRES_DEV_PASSWORD = os.environ["POSTGRES_DEV_PASSWORD"]
+		except:
+			print("Could not connect to Postgres, edit password in: ProSpector\backend\dev_backend\SQL\config.py")
+			"""self.POSTGRES_DEV_PASSWORD ="Mitt personlige passord"""
+		self.DATABASE_URL = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_DEV_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+		
+		''' ___ EMAIL ___ '''
 		self.MAIL_FROM = os.environ["MAIL_FROM"]
 		self.MAIL_PORT = os.environ["MAIL_PORT"]
 		self.MAIL_SERVER = os.environ["MAIL_SERVER"]
 		self.MAIL_FROM_NAME = os.environ["MAIL_SERVER"]
 		self.MAIL_USERNAME = os.environ["MAIL_USERNAME"]
 		self.MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
-		# self.MAIL_TSL = os.environ["MAIL_TSL"]
-		# self.MAIL_SSL = os.environ["MAIL_SSL"]
 		self.MAIL_TSL = os.environ["MAIL_TSL"]
 		self.MAIL_SSL = os.environ["MAIL_SSL"]
 		self.USE_CREDENTIALS = os.environ["USE_CREDENTIALS"]	
+
+		''' ___ TWILIO ___ '''
 		self.TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
 		self.TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
-		self.VERIFY_SID = 'VA0af259d1f90eef02d4cfe86f45e64b8f'
+		self.VERIFY_SID = 'VA0af259d1f90eef02d4cfe86f45e64b8f'	
 
 	@property
 	def getMailConnection(self) -> FastMail:
@@ -133,25 +132,6 @@ class Settings:
 				VALIDATE_CERTS = True
 			)
 		return FastMail(conf) # fm 
-	
-	
-	# TEMPORARLY DISABLED WHILE TESTING (MIGHT BE REMOVED)
-	# @property
-	# def getPostgresConnection(self) -> Generator: # <-TEMP
-	# 	'''
-	# 	returns a connection to Postgres Server, -> db 
-	# 	'''
-	# 	engine = create_engine(self.DATABASE_URL)
-	# 	SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
-	# 	try:
-	# 		db = SessionLocal()
-	# 		yield db
-	# 	finally:
-	# 		db.close()
-
-	# @property
-	# def getBase(self) -> declarative_base:
-	# 	return declarative_base()
 
 class User:
 	settings = Settings()
@@ -164,9 +144,6 @@ class User:
 		
 	engine = create_engine(settings.DATABASE_URL)
 	base = declarative_base()
-
-
-
 
 
 #!! SHOULD PROB REMOVE THIS 
